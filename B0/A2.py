77034b0fad2a7dc3a10588461e9fd3b4b1ef682e
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2023-10-20 08:56:35 +0200

    Erster Aufgabenteil

--- B0/A2.py
+++ B0/A2.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 
+import pandas as pd
+import numpy as np
+
 # Erstellen sie ein Dataframe  mit 10^5 uniform zwischen 0 und 1 verteilten Zufallszahlen x_1, x_2.
+num_randoms = 10**5
+data = pd.DataFrame(
+    {"x_1": np.random.rand(num_randoms), "x_2": np.random.rand(num_randoms)}
+)
+# print(data)
